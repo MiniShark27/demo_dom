9719d1daeb0da7b11449a365680aef59b709f116
MiniShark27 <kobifalus@gmail.com>
2022-02-02 13:41:54 -0500

    script link and login

---
+++ public/login.js
@@ -0,0 +1,9 @@
+function login() {
+  console.log("Hello")
+  const username_input = document.getElementById('username');
+  const username = username_input.value
+  const password_input = document.getElementById('pass');
+  const password = password_input.value
+
+  console.log(username, password)
+}
